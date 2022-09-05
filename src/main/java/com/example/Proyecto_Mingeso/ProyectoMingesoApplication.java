641db437fd18da25cecfccc0d7805480329fdc21
Maximiliano Ahumada <64162650+Maxahup@users.noreply.github.com>
2022-09-05 15:05:56 -0300

    Creacion del proyecto

---
+++ src/main/java/com/example/Proyecto_Mingeso/ProyectoMingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Mingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMingesoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Proyecto_Mingeso/ProyectoMingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Mingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
