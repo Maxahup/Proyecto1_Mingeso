a3832465708f5c6cbbfbc25d03ecd6557cefd39d
Maximiliano Ahumada <maximiliano.ahumada@usach.cl>
2022-09-14 22:41:11 -0300

    incio proceso de carga de archivo data

---
+++ src/main/java/Services/CargaData.java
@@ -0,0 +1,12 @@
+package Services;
+
+import java.io.File;
+
+public class CargaData {
+
+    public void cargarData(String nombreArchivo){
+        File data = new File("ruta_archivo");
+
+    }
+
+}
